71158b3727676e3f7eaad22e86f223e794be717b
Gaurav-Patel22 <patelgaurav5190@gmail.com>
2026-03-11 18:20:36 +0530

    first commit

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "172.31.18.203",
-    user: "root",
-    password: "Gaurav@22092005",
-    database: "pharma_cloudops"
+  host: "172.31.18.203",
+  user: "pharma_user",
+  password: "Gaurav@22092005",
+  database: "pharma_cloudops"
 });
 
 db.connect((err) => {
