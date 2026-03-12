bac877a29c10fe32100bd256e9f4eb82fa3eb66b
Gaurav-Patel22 <patelgaurav5190@gmail.com>
2026-03-12 15:09:44 +0530

    first commit

--- public/login.js
+++ public/login.js
@@ -2,7 +2,7 @@ function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    fetch("http://34.236.189.241:3000/login", {
+    fetch("http://34.236.189.241:3000//login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
