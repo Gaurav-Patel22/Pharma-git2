e873eaaa01d4c72f8ba8841c685fe9256aed200c
Gaurav-Patel22 <patelgaurav5190@gmail.com>
2026-03-16 15:40:29 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require("./db");
 
 const app = express();
 app.use(cors());
-app.use(express.json());    
+app.use(express.json());
 
 // ================= MIDDLEWARE =================
 app.use(cors());
